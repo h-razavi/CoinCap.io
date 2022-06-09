4387992ca226ea51a27d4a301313b76de1cf664d
Hossain Razavi <hossain.razavi@gmail.com>
2022-06-09 10:47:08 +0430

    table rendered

---
+++ main.js
@@ -0,0 +1,84 @@
+//fetch data
+let data = [];
+function getTableData(){
+    return fetch("https://api.coincap.io/v2/assets")
+    .then(function(res){
+        return res.json();
+    }).then(function(assets){
+        data=assets.data
+        return data;
+    })
+}
+
+
+//DOM manipulation
+let dataContainer = document.querySelector('tbody');
+function renderTable(rank,name,symbol,price,cap,vwap,supply,volume,change){
+    let tableRow = document.createElement('tr');
+
+    let rankCell = document.createElement('td');
+    rankCell.classList.add('table-center-align');
+    rankCell.textContent = rank;
+
+    let nameCell = document.createElement('td');
+    nameCell.classList.add('table-left-align');
+    nameCell.setAttribute('colspan','2');
+    let addLink = document.createElement('a');
+    addLink.setAttribute('href','#');
+    addLink.textContent= name;
+    let short = document.createElement('p');
+    short.classList.add('coin-sym');
+    short.textContent= symbol;
+    addLink.appendChild(short);
+    nameCell.appendChild(addLink);
+
+    let priceCell = document.createElement('td');
+    priceCell.textContent = price;
+
+    let capCell = document.createElement('td');
+    capCell.textContent = cap;
+
+    let vwapCell = document.createElement('td');
+    vwapCell.textContent = vwap;
+
+    let supplyCell = document.createElement('td');
+    supplyCell.textContent = supply;
+
+    let volumeCell = document.createElement('td');
+    volumeCell.textContent = volume;
+
+    let changeCell = document.createElement('td');
+    changeCell.textContent = change;
+
+    tableRow.appendChild(rankCell);
+    tableRow.appendChild(nameCell);
+    tableRow.appendChild(priceCell);
+    tableRow.appendChild(capCell);
+    tableRow.appendChild(vwapCell);
+    tableRow.appendChild(supplyCell);
+    tableRow.appendChild(volumeCell);
+    tableRow.appendChild(changeCell);
+
+
+    dataContainer.appendChild(tableRow);
+}
+
+
+function importTable(table){
+    table.forEach(function (item){
+        renderTable(item.rank,item.name,item.symbol,item.priceUsd,item.marketCapUsd,item.vwap24Hr,item.supply,item.volumeUsd24Hr,item.changePercent24Hr);
+    })
+}
+
+getTableData().then(function(){
+    importTable(data);
+})
+
+
+
+
+
+
+
+
+
